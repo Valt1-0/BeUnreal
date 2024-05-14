6b3f7d8772873a6658a044c67c05bdc940b69c84
Valt1-0 <lai2val1@gmail.com>
2024-05-14 17:37:19 +0200

    chore: Add StatusBar styling to set the style to Dark

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,19 @@ import App from './App';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { firebaseConfig } from './db/Firebase';
+import { StatusBar, Style } from '@capacitor/status-bar';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 console.log(analytics);
 
+
+const setStatusBarStyleDark = async () => {
+  await StatusBar.setStyle({ style: Style.Dark });
+};
+
+setStatusBarStyleDark();
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
