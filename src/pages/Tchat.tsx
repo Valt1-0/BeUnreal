d9f10e75aa985026fdc1dd26ea0fba2b6ea7a68f
Valt1-0 <lai2val1@gmail.com>
2024-05-16 10:44:49 +0200

    chore: Add user avatar to Tchat page

--- src/pages/Tchat.tsx
+++ src/pages/Tchat.tsx
@@ -75,6 +75,14 @@ const Tchat: React.FC = () => {
                         : "chat-start"
                     }`}
                   >
+                    <div className="chat-image avatar">
+                      <div className="w-10 rounded-full">
+                        <img
+                          alt="Tailwind CSS chat bubble component"
+                          src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                        />
+                      </div>
+                    </div>
                     <div className="chat-header">
                       {msg.senderId === authenticatedUser.uid
                         ? "You"
