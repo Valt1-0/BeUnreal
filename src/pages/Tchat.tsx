3838abc13ebf5b9f6147f2096c9782c96c62e85d
Valt1-0 <lai2val1@gmail.com>
2024-05-16 10:40:56 +0200

    chore: Update Tchat page with message sorting and formatting functions

--- src/pages/Tchat.tsx
+++ src/pages/Tchat.tsx
@@ -1,70 +1,105 @@
 import React, { useState, useEffect } from "react";
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-  IonInput,
-  IonButton,
-  IonList,
-  IonItem,
-} from "@ionic/react";
+import { IonContent, IonPage, IonButton, IonToolbar } from "@ionic/react";
 import { MobXProviderContext, observer } from "mobx-react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import { Message } from "../store/Firebase";
-import {serverTimestamp} from "firebase/firestore";
+import { serverTimestamp } from "firebase/firestore";
 
 const Tchat: React.FC = () => {
-    const { id } = useParams<{ id: string }>();
+  const { id } = useParams<{ id: string }>();
   const [message, setMessage] = useState("");
-    const { store } = React.useContext(MobXProviderContext);
-    let { authenticatedUser, initializationError, tchatMessages } = store;
-
+  const { store } = React.useContext(MobXProviderContext);
+  const { authenticatedUser, tchatMessages } = store;
 
   useEffect(() => {
-    const fetchTchatMessage = async  () => {
-     await  store.getTchatMessages(id, authenticatedUser.uid);
-    }
+    const fetchTchatMessage = async () => {
+      await store.getTchatMessages(id, authenticatedUser.uid);
+    };
     fetchTchatMessage();
-
   }, []);
 
   useEffect(() => {
     console.log("message : ", message);
   }, [message]);
-const sendMessage = async () => {
-  const newMessage: Message = {
-    senderId: authenticatedUser.uid,
-    content: message,
-    timestamp: serverTimestamp,
-    type: "text",
+
+  // Fonction pour trier les messages par timestamp
+  const sortMessagesByTimestamp = (messages: Message[]) => {
+    return messages.slice().sort((a, b) => {
+      if (a.timestamp && b.timestamp) {
+        return a.timestamp.seconds - b.timestamp.seconds;
+      }
+      return 0;
+    });
   };
 
-  try {
-    await store.doSendMessage(id, newMessage);
-    setMessage("");
-  } catch (err) {
-    console.error(err);
-  }
-};
+  // Fonction pour formater le timestamp en heure HH:MM
+  const formatTimestamp = (timestamp: any) => {
+    const date = new Date(timestamp.seconds * 1000);
+    return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+  };
+
+  // Fonction pour envoyer un message
+  const sendMessage = async () => {
+    const newMessage: Message = {
+      senderId: authenticatedUser.uid,
+      content: message,
+      timestamp: serverTimestamp(),
+      type: "text",
+    };
+
+    try {
+      await store.doSendMessage(id, newMessage);
+      setMessage("");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <IonPage>
       <IonContent>
-        <IonList>
-          {tchatMessages.map((msg: string, index: number) => (
-            <IonItem key={index}>{msg.content}</IonItem>
-          ))}
-        </IonList>
-        <div className="flex items-center py-20">
-          <input
-            placeholder="Enter Message"
-            className="flex-grow mr-4 text-black border-2 border-gray-300 p-2 rounded-lg"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-          <IonButton onClick={sendMessage}>Send</IonButton>
+        <div className="h-full flex flex-col justify-center items-center">
+          <div className="w-screen overflow-y-scroll">
+            {sortMessagesByTimestamp(tchatMessages).map(
+              (msg: Message, index: number) => (
+                <div
+                  key={index}
+                  className={`flex flex-col items-${
+                    msg.senderId === authenticatedUser.uid ? "end" : "start"
+                  }`}
+                >
+                  <div
+                    className={`chat ${
+                      msg.senderId === authenticatedUser.uid
+                        ? "chat-end"
+                        : "chat-start"
+                    }`}
+                  >
+                    <div className="chat-header">
+                      {msg.senderId === authenticatedUser.uid
+                        ? "You"
+                        : "Other User"}
+                    </div>
+                    <div className="chat-bubble">{msg.content}</div>
+                    <div className="chat-footer">
+                      <time className="text-xs opacity-50">
+                        {formatTimestamp(msg.timestamp)}
+                      </time>
+                    </div>
+                  </div>
+                </div>
+              )
+            )}
+          </div>
+          <div className="flex items-center py-20">
+            <input
+              placeholder="Enter Message"
+              className="mx-4 text-white border-2 border-white font-eloquiabold p-2 rounded-lg"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <IonButton onClick={sendMessage}>Send</IonButton>
+          </div>
         </div>
       </IonContent>
     </IonPage>
