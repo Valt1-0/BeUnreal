041a4c3384d20f238372c2da801bf252b9b436cc
Valt1-0 <lai2val1@gmail.com>
2024-05-27 02:27:32 +0200

    chore: Update date and time formatting options in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,11 +29,18 @@ function convertTimestamp(timestamp: {
     timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000
   );
 
-  const optionsDate = { day: "2-digit", month: "2-digit", year: "numeric" };
-  const optionsTime = { hour: "2-digit", minute: "2-digit" };
+  const optionsDate: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  };
+  const optionsTime: Intl.DateTimeFormatOptions = {
+    hour: "2-digit",
+    minute: "2-digit",
+  };
 
-  const formattedDate = date.toLocaleDateString([], optionsDate);
-  const formattedTime = date.toLocaleTimeString([], optionsTime);
+  const formattedDate = date.toLocaleDateString(undefined, optionsDate);
+  const formattedTime = date.toLocaleTimeString(undefined, optionsTime);
 
   return { date: formattedDate, time: formattedTime };
 }
